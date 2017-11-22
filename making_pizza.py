35985d7f20351200693317f9511da5d8e29fd9ca
dhlee070 <dhlee070@gmail.com>
2017-11-22 21:30:05 +0900

    printing

--- making_pizza.py
+++ making_pizza.py
@@ -2,3 +2,4 @@ from pizza import *
 
 make_pizza(16, 'pepperoni')
 make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+print("hey")
--- my_electric_car.py
+++ my_electric_car.py
@@ -8,3 +8,4 @@ print(my_beetle.get_descriptive_name())
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
 my_tesla.battery.get_range()
+###
